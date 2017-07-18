b2817efc314642dd9b6f595c9c8416964f982632
Jonathan Fischoff <jfischoff@yahoo.com>
2017-07-17 23:49:36 -0400

    Expanded options. Removed default for Options. Fixed up tests

--- src/Database/PostgreSQL/Simple/Options.hs
+++ src/Database/PostgreSQL/Simple/Options.hs
@@ -3,6 +3,7 @@
 -}
 {-# LANGUAGE RecordWildCards, LambdaCase, DeriveGeneric, DeriveDataTypeable #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving, CPP, GADTs, OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Database.PostgreSQL.Simple.Options where
 import Database.PostgreSQL.Simple
 import Options.Applicative
@@ -20,17 +21,61 @@ import URI.ByteString as URI
 import Control.Monad
 import Data.List.Split
 import Data.List (intercalate)
+import Generics.Deriving.Monoid
+import Data.Char
+import Data.Maybe
 
-type Options = ConnectInfo
-
+data Options = Options
+  { oHost                    :: Maybe String
+  , oHostaddr                :: Maybe String
+  , oPort                    :: Int
+  , oUser                    :: String
+  , oPassword                :: String
+  , oDbname                  :: String
+  , oConnectTimeout          :: Maybe Int
+  , oClientEncoding          :: Maybe String
+  , oOptions                 :: Maybe String
+  , oFallbackApplicationName :: Maybe String
+  , oKeepalives              :: Maybe Int
+  , oKeepalivesIdle          :: Maybe Int
+  , oKeepalivesCount         :: Maybe Int
+  , oSslmode                 :: Maybe String
+  , oRequiressl              :: Maybe Int
+  , oSslcompression          :: Maybe Int
+  , oSslcert                 :: Maybe String
+  , oSslkey                  :: Maybe String
+  , oSslrootcert             :: Maybe String
+  , oRequirepeer             :: Maybe String
+  , oKrbsrvname              :: Maybe String
+  , oGsslib                  :: Maybe String
+  , oService                 :: Maybe String
+  } deriving (Show, Eq, Read, Ord, Generic, Typeable)
 -- | An optional version of 'Options'. This includes an instance of
 -- | 'ParseRecord' which provides the optparse-applicative Parser.
 data PartialOptions = PartialOptions
-  { host     :: Last String
-  , port     :: Last Int
-  , user     :: Last String
-  , password :: Last String
-  , database :: Last String
+  { host                    :: Last String
+  , hostaddr                :: Last String
+  , port                    :: Last Int
+  , user                    :: Last String
+  , password                :: Last String
+  , dbname                  :: Last String
+  , connectTimeout          :: Last Int
+  , clientEncoding          :: Last String
+  , options                 :: Last String
+  , fallbackApplicationName :: Last String
+  , keepalives              :: Last Int
+  , keepalivesIdle          :: Last Int
+  , keepalivesCount         :: Last Int
+  , sslmode                 :: Last String
+  , requiressl              :: Last Int
+  , sslcompression          :: Last Int
+  , sslcert                 :: Last String
+  , sslkey                  :: Last String
+  , sslrootcert             :: Last String
+  , requirepeer             :: Last String
+  , krbsrvname              :: Last String
+  , gsslib                  :: Last String
+  , service                 :: Last String
   } deriving (Show, Eq, Read, Ord, Generic, Typeable)
 
 instance ParseRecord PartialOptions where
@@ -38,16 +83,17 @@ instance ParseRecord PartialOptions where
     <|> parseRecordWithModifiers defaultModifiers
 
 instance Monoid PartialOptions where
-  mempty = PartialOptions (Last Nothing) (Last Nothing)
-                              (Last Nothing) (Last Nothing)
-                              (Last Nothing)
-  mappend x y = PartialOptions
-    { host     = host     x <> host     y
-    , port     = port     x <> port     y
-    , user     = user     x <> user     y
-    , password = password x <> password y
-    , database = database x <> database y
-    }
+  mempty = gmemptydefault
+  mappend = gmappenddefault
+
+-- Copied from Options.Generic source code
+underscoreModifiers :: Modifiers
+underscoreModifiers = Modifiers lispCase lispCase
+  where
+    lispCase = dropWhile (== '_') . (>>= lower) . dropWhile (== '_')
+    lower c | isUpper c = ['_', toLower c]
+            | otherwise = [c]
+
 
 unSingleQuote :: String -> Maybe String
 unSingleQuote (x : xs@(_ : _))
@@ -63,12 +109,12 @@ mkLast = Last . Just
 
 -- | The 'PartialOptions' version of 'defaultOptions'
 instance Default PartialOptions where
-    def = PartialOptions
+    def = mempty
       { host     = mkLast $                connectHost     defaultConnectInfo
       , port     = mkLast $ fromIntegral $ connectPort     defaultConnectInfo
       , user     = mkLast $                connectUser     defaultConnectInfo
       , password = mkLast $                connectPassword defaultConnectInfo
-      , database = mkLast $                connectDatabase defaultConnectInfo
+      , dbname = mkLast $                  connectDatabase defaultConnectInfo
       }
 
 getOption :: String -> Last a -> Validation [String] a
@@ -77,13 +123,64 @@ getOption optionName = \case
     Last Nothing  -> Data.Either.Validation.Failure
         ["Missing " ++ optionName ++ " option"]
 
+getLast' :: Applicative f => Last a -> f (Maybe a)
+getLast' = pure . getLast
+
 completeOptions :: PartialOptions -> Either [String] Options
 completeOptions PartialOptions {..} = validationToEither $ do
-  ConnectInfo <$> getOption "host"     host
-              <*> (fromIntegral <$> getOption "port" port)
-              <*> getOption "user"     user
-              <*> getOption "password" password
-              <*> getOption "database" database
+  Options <$> getLast' host
+          <*> getLast' hostaddr
+          <*> (fromIntegral <$> getOption "port" port)
+          <*> getOption "user"     user
+          <*> getOption "password" password
+          <*> getOption "dbname" dbname
+          <*> getLast' connectTimeout
+          <*> getLast' clientEncoding
+          <*> getLast' options
+          <*> getLast' fallbackApplicationName
+          <*> getLast' keepalives
+          <*> getLast' keepalivesIdle
+          <*> getLast' keepalivesCount
+          <*> getLast' sslmode
+          <*> getLast' requiressl
+          <*> getLast' sslcompression
+          <*> getLast' sslcert
+          <*> getLast' sslkey
+          <*> getLast' sslrootcert
+          <*> getLast' requirepeer
+          <*> getLast' krbsrvname
+          <*> getLast' gsslib
+          <*> getLast' service
+
+maybeToPair :: Show a => String -> Maybe a -> [(String, String)]
+maybeToPair k mv = (\v -> (k, show v)) <$> maybeToList mv
+
+toConnectionString :: Options -> ByteString
+toConnectionString Options {..} = BSC.pack $ unwords $ map (\(k, v) -> k <> "=" <> v)
+  $  maybeToPair "host" oHost
+  <> maybeToPair "hostaddr" oHostaddr
+  <> [ ("port", show oPort)
+     , ("user", oUser)
+     , ("password", oPassword)
+     , ("dbname", oDbname)
+     ]
+  <> maybeToPair "connect_timeout" oConnectTimeout
+  <> maybeToPair "client_encoding" oClientEncoding
+  <> maybeToPair "options" oOptions
+  <> maybeToPair "fallback_applicationName" oFallbackApplicationName
+  <> maybeToPair "keepalives" oKeepalives
+  <> maybeToPair "keepalives_idle" oKeepalivesIdle
+  <> maybeToPair "keepalives_count" oKeepalivesCount
+  <> maybeToPair "sslmode" oSslmode
+  <> maybeToPair "requiressl" oRequiressl
+  <> maybeToPair "sslcompression" oSslcompression
+  <> maybeToPair "sslcert" oSslcert
+  <> maybeToPair "sslkey" oSslkey
+  <> maybeToPair "sslrootcert" oSslrootcert
+  <> maybeToPair "requirepeer" oRequirepeer
+  <> maybeToPair "krbsrvname" oKrbsrvname
+  <> maybeToPair "gsslib" oGsslib
+  <> maybeToPair "service" oService
 
 
 -- | Useful for testing or if only Options are needed.
@@ -93,7 +190,7 @@ completeParser =
 
 -- | Create a connection with an 'Option'
 run :: Options -> IO Connection
-run = connect
+run = connectPostgreSQL . toConnectionString
 
 userInfoToPartialOptions :: UserInfo -> PartialOptions
 userInfoToPartialOptions UserInfo {..} = mempty { user = return $ BSC.unpack uiUsername } <> if BS.null uiPassword
@@ -108,19 +205,52 @@ autorityToPartialOptions Authority {..} = maybe mempty userInfoToPartialOptions 
 pathToPartialOptions :: ByteString -> PartialOptions
 pathToPartialOptions path = case drop 1 $ BSC.unpack path of
   "" -> mempty
-  x  -> mempty {database = return x }
+  x  -> mempty {dbname = return x }
+
+parseInt :: String -> String -> Either String Int
+parseInt msg v = maybe (Left (msg <> " value of: " <> v <> " is not a number")) Right $
+      readMaybe v
 
 keywordToPartialOptions :: String -> String -> Either String PartialOptions
 keywordToPartialOptions k v = case k of
   "host" -> return $ mempty { host = return $ v }
+  "hostaddress" -> return $ mempty { hostaddr = return $ v }
   "port" -> do
-    portValue <- maybe (Left ("port value of: " <> v <> " is not a number")) Right $
-      readMaybe v
-
+    portValue <- parseInt "port" v
     return $ mempty { port = return portValue }
   "user" -> return $ mempty { user = return v }
   "password" -> return $ mempty { password = return v }
-  "dbname" -> return $ mempty { database = return v}
+  "dbname" -> return $ mempty { dbname = return v}
+  "connect_timeout" -> do
+    x <- parseInt "connect_timeout" v
+    return $ mempty { connectTimeout = return x }
+  "client_encoding" -> return $ mempty { clientEncoding = return v }
+  "options" -> return $ mempty { options = return v }
+  "fallback_applicationName" -> return $ mempty { fallbackApplicationName = return v }
+  "keepalives" -> do
+    x <- parseInt "keepalives" v
+    return $ mempty { keepalives = return x }
+  "keepalives_idle" -> do
+    x <- parseInt "keepalives_idle" v
+    return $ mempty { keepalivesIdle = return x }
+  "keepalives_count" -> do
+    x <- parseInt "keepalives_count" v
+    return $ mempty { keepalivesCount = return x }
+  "sslmode" -> return $ mempty { sslmode = return v }
+  "requiressl" -> do
+    x <- parseInt "requiressl" v
+    return $ mempty { requiressl = return x }
+  "sslcompression" -> do
+    x <- parseInt "sslcompression" v
+    return $ mempty { sslcompression = return x }
+  "sslcert" -> return $ mempty { sslcert = return v }
+  "sslkey" -> return $ mempty { sslkey = return v }
+  "sslrootcert" -> return $ mempty { sslrootcert = return v }
+  "requirepeer" -> return $ mempty { requirepeer = return v }
+  "krbsrvname" -> return $ mempty { krbsrvname = return v }
+  "gsslib" -> return $ mempty { gsslib = return v }
+  "service" -> return $ mempty { service = return v }
+
   x -> Left $ "Unrecongnized option: " ++ show x
 
 queryToPartialOptions :: URI.Query -> Either String PartialOptions
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings, CPP #-}
 import Test.Hspec
 import Database.PostgreSQL.Simple.Options
-import Database.PostgreSQL.Simple
 import System.Environment
 import Options.Applicative
 import System.Exit
+import Data.Default
 #if !MIN_VERSION_base(4,8,0)
 import Data.Monoid
 #endif
@@ -20,66 +20,66 @@ main = hspec $ do
                      , "--port=1234"
                      , "--user=nobody"
                      , "--password=everytimeiclosemyeyes"
-                     , "--database=future"
+                     , "--dbname=future"
                      ]
 
-          expected = ConnectInfo
-                     { connectHost     = "example.com"
-                     , connectPort     = 1234
-                     , connectUser     = "nobody"
-                     , connectPassword = "everytimeiclosemyeyes"
-                     , connectDatabase = "future"
+          expected = completeOptions $ mempty
+                     { host     = return "example.com"
+                     , port     = return 1234
+                     , user     = return "nobody"
+                     , password = return "everytimeiclosemyeyes"
+                     , dbname   = return "future"
                      }
 
       actual <- withArgs testArgs testParser
-      actual `shouldBe` expected
+      Right actual `shouldBe` expected
 
     it "parses some and uses defaults for others" $ do
       let testArgs = [ "--user=nobody"
                      , "--password=everytimeiclosemyeyes"
-                     , "--database=future"
+                     , "--dbname=future"
                      ]
 
-          expected = ConnectInfo
-                     { connectHost     = "127.0.0.1"
-                     , connectPort     = 5432
-                     , connectUser     = "nobody"
-                     , connectPassword = "everytimeiclosemyeyes"
-                     , connectDatabase = "future"
+          expected = completeOptions $ mempty
+                     { host     = return "127.0.0.1"
+                     , port     = return 5432
+                     , user     = return "nobody"
+                     , password = return "everytimeiclosemyeyes"
+                     , dbname   = return "future"
                      }
 
       actual <- withArgs testArgs testParser
-      actual `shouldBe` expected
+      Right actual `shouldBe` expected
 
     it "parses no options and gives defaults" $ do
-      let expected = ConnectInfo
-                     { connectHost     = "127.0.0.1"
-                     , connectPort     = 5432
-                     , connectUser     = "postgres"
-                     , connectPassword = ""
-                     , connectDatabase = ""
+      let expected = completeOptions $ mempty
+                     { host     = return "127.0.0.1"
+                     , port     = return 5432
+                     , user     = return "postgres"
+                     , password = return ""
+                     , dbname   = return ""
                      }
 
       actual <- withArgs [] testParser
-      actual `shouldBe` expected
+      Right actual `shouldBe` expected
     it "parses the connection string double quoted" $ do
       let testArgs = ["--connectString=\"host=yahoo\""]
-          expected = defaultConnectInfo { connectHost = "yahoo" }
+          expected = completeOptions $ def { host = return "yahoo" }
 
       actual <- withArgs testArgs testParser
-      actual `shouldBe` expected
+      Right actual `shouldBe` expected
     it "parses the connection string single quoted" $ do
       let testArgs = ["--connectString='host=yahoo'"]
-          expected = defaultConnectInfo { connectHost = "yahoo" }
+          expected = completeOptions $ def { host = return "yahoo" }
 
       actual <- withArgs testArgs testParser
-      actual `shouldBe` expected
+      Right actual `shouldBe` expected
     it "parses the connection string no quotes" $ do
       let testArgs = ["--connectString=host=yahoo"]
-          expected = defaultConnectInfo { connectHost = "yahoo" }
+          expected = completeOptions $ def { host = return "yahoo" }
 
       actual <- withArgs testArgs testParser
-      actual `shouldBe` expected
+      Right actual `shouldBe` expected
     it "fails if connectString and other args are passed" $ do
       let testArgs = ["--connectString=host=yahoo", "--port=1234"]
           handler :: ExitCode -> Bool
@@ -96,37 +96,37 @@ main = hspec $ do
 
     it "parses all keywords" $ parseConnectionString
       "host=localhost port=1234 user=jonathan password=open dbname=dev" `shouldBe`
-        Right (PartialOptions
+        Right (mempty
           { host = return "localhost"
           , port = return 1234
           , user = return "jonathan"
           , password = return "open"
-          , database = return "dev"
+          , dbname = return "dev"
           })
 
     it "parses host only connection string" $ parseConnectionString "postgresql://localhost"
       `shouldBe` Right (mempty { host = return "localhost" })
 
     it "parses all params" $ parseConnectionString "postgresql://jonathan:open@localhost:1234/dev"
-      `shouldBe` Right (PartialOptions
+      `shouldBe` Right (mempty
           { host = return "localhost"
           , port = return 1234
           , user = return "jonathan"
           , password = return "open"
-          , database = return "dev"
+          , dbname = return "dev"
           })
 
     it "parses all params using query params" $ parseConnectionString "postgresql:///dev?host=localhost&port=1234"
       `shouldBe` Right (mempty
         { host = return "localhost"
         , port = return 1234
-        , database = return "dev"
+        , dbname = return "dev"
         })
 
     it "decodes a unix port" $ parseConnectionString "postgresql://%2Fvar%2Flib%2Fpostgresql/dbname"
       `shouldBe` Right (mempty
           { host = return "/var/lib/postgresql"
-          , database = return "dbname"
+          , dbname = return "dbname"
           })
 
 
